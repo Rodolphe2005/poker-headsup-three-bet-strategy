2b029c3d81b45eac68d1fdb92f19e2f1c1875062
Rodolphe <rodolphe@lalilo.com>
2018-08-30 08:31:28 +0200

    Fixed initialization of FullHouse

--- showdown_strengths/full_house.py
+++ showdown_strengths/full_house.py
@@ -8,9 +8,9 @@ from hand import Hand
 class FullHouse:
     ranking = 7
 
-    def __init__(self, trips: CardNumber, pair: CardNumber):
-        self.trips = trips
-        self.pair = pair
+    def __init__(self, trips: str, pair: str):
+        self.trips = CardNumber(trips)
+        self.pair = CardNumber(pair)
 
     def __repr__(self):
         return f'FullHouse({self.trips.name}, {self.pair.name})'
@@ -26,8 +26,8 @@ def full_house_of(hand: Hand, board: Board):
     cards = hand.cards + board.cards
     counter = Counter([card.number for card in cards])
     if 2 in counter.values() and 3 in counter.values():
-        possible_trips = [card_number for card_number, count in counter.items() if count == 3]
-        possible_pairs = [card_number for card_number, count in counter.items() if count == 2]
+        possible_trips = [card_number.name for card_number, count in counter.items() if count == 3]
+        possible_pairs = [card_number.name for card_number, count in counter.items() if count == 2]
         assert len(possible_trips) == 1
         assert 1 <= len(possible_pairs) <= 2
         return FullHouse(max(possible_trips), max(possible_pairs))
--- showdown_strengths/full_house_test.py
+++ showdown_strengths/full_house_test.py
@@ -5,18 +5,18 @@ from showdown_strengths.full_house import FullHouse, full_house_of
 
 
 def test_full_house_initialization():
-    full_house = FullHouse(CardNumber('Q'), CardNumber('J'))
-    assert full_house.trips == CardNumber('Q')
-    assert full_house.pair == CardNumber('J')
+    full_house = FullHouse('Q', 'J')
+    assert full_house.trips.name == 'Q'
+    assert full_house.pair.name == 'J'
 
 
 def test_full_house_comparison():
-    assert FullHouse(CardNumber('Q'), CardNumber('J')) < FullHouse(CardNumber('K'), CardNumber('J'))
-    assert FullHouse(CardNumber('Q'), CardNumber('J')) < FullHouse(CardNumber('Q'), CardNumber('A'))
-    assert FullHouse(CardNumber('Q'), CardNumber('J')) == FullHouse(CardNumber('Q'), CardNumber('J'))
+    assert FullHouse('Q', 'J') < FullHouse('K', 'J')
+    assert FullHouse('Q', 'J') < FullHouse('Q', 'A')
+    assert FullHouse('Q', 'J') == FullHouse('Q', 'J')
 
 
 def test_full_house_of():
-    expected = FullHouse(CardNumber('Q'), CardNumber('T'))
+    expected = FullHouse('Q', 'T')
     actual = full_house_of(Hand('QhQs'), Board('QcThTc9h8s'))
     assert expected == actual

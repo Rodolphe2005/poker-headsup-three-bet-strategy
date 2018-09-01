871518a0d724504384119463398f7eb2d1e41226
Rodolphe <rodolphe@lalilo.com>
2018-09-01 06:38:12 +0200

    remove useless files

--- hand_battle/board_test.py
+++
@@ -1,7 +0,0 @@
-from hand_battle.board import Board
-from hand_battle.card import Card
-
-
-def test_board_initialization():
-    board = Board('2h3h4h5h6h')
-    assert [Card(str(i) + 'h') for i in range(2, 7)] == board.cards

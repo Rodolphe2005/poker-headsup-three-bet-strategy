68f01a4cbd2a8c0b2c42913fd1fa634db743540c
Rodolphe <rodolphe@lalilo.com>
2018-09-16 23:07:55 +0200

    With input ranges for steal and fold to 3bet percentages

--- three_bet_strategy/export_all_hands_to_json.py
+++ three_bet_strategy/export_all_hands_to_json.py
@@ -17,18 +17,34 @@ def generate_hand_properties(steal, fold_percentage):
         hand = Hand(name=card1 + card2 + possible_suit, combos=combos_of[possible_suit])
         yield {
             "name": hand.name,
-            "i": 13-index1,
-            "j": 13-index2,
+            "i": 13 - index1,
+            "j": 13 - index2,
             "call_value": call_equity(hand, steal=steal),
             "three_bet_value": three_bet_equity(hand=hand, steal=steal, fold_percentage=fold_percentage),
+            "steal": steal,
+            "fold_percentage": fold_percentage,
         }
 
 
 app = Flask('myapp', template_folder=current_folder)
 with app.app_context():
+    min_steal = 60
+    max_steal = 80
+    min_fold = 45
+    max_fold=55
+
     html_page = render_template(
         'three_bet_strategy.html',
-        data_points=list(generate_hand_properties(steal=80, fold_percentage=25))
+        data_points=[
+            hand_as_json
+            for steal in range(min_steal, max_steal+1, 1)
+            for fold_percentage in range(min_fold, max_fold, 1)
+            for hand_as_json in generate_hand_properties(steal=steal, fold_percentage=fold_percentage)
+        ],
+        min_steal=min_steal,
+        max_steal=max_steal,
+        min_fold=min_fold,
+        max_fold=max_fold
     )
 
     with open(current_folder + '/generated_three_bet_strategy.html', 'w') as fp:

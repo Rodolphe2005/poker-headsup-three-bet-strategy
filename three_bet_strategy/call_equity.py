713dcbb808b715348841f097fb686226319b9882
Rodolphe <rodolphe@lalilo.com>
2018-09-18 14:10:15 +0200

    raise_size and three_bet_size are now variables

--- three_bet_strategy/call_equity.py
+++ three_bet_strategy/call_equity.py
@@ -2,10 +2,14 @@ from order_poker_hands import get_top_x_percent_of_hands
 from range_battle import range_battle
 
 
+raise_size = 2.5
+three_bet_size = 8
+four_bet_percentage = 10
+
 def call_equity(hand, steal):
     sb_range = list(get_top_x_percent_of_hands(steal))
     equity_on_the_flop = range_battle([hand], sb_range)
-    return 5 * equity_on_the_flop - 2.5
+    return 2 * raise_size * equity_on_the_flop - raise_size
 
 
 def fold_equity():
@@ -14,10 +18,14 @@ def fold_equity():
 
 def three_bet_equity(hand, steal, fold_percentage):
     # If he folds, we win 2.5
-    equity = fold_percentage / 100 * 2.5
+    equity = fold_percentage / 100 * raise_size
 
     # If he calls
+    # TODO Remove the top percent that corresponds to his 4Bet range
     sb_call_range = list(get_top_x_percent_of_hands(steal * (100 - fold_percentage) / 100))
     equity_on_the_flop = range_battle([hand], sb_call_range)
-    equity += (100 - fold_percentage) / 100 * (16 * equity_on_the_flop - 8)
+    equity += (100 - fold_percentage - four_bet_percentage) / 100 * (2 * three_bet_size * equity_on_the_flop - three_bet_size)
+
+    # If he 4bets, we suppose that we always fold
+    equity -= four_bet_percentage/100 * three_bet_size
     return equity
--- three_bet_strategy/export_all_hands_to_json.py
+++ three_bet_strategy/export_all_hands_to_json.py
@@ -3,7 +3,9 @@ from flask import render_template, Flask
 
 from hand import Hand
 from hand_battle import possible_hands, possible_cards, combos_of
+from range_battle import range_battle
 from three_bet_strategy.call_equity import three_bet_equity, call_equity
+from utils import do_profile
 
 current_folder = os.path.dirname(__file__)
 
@@ -26,26 +28,30 @@ def generate_hand_properties(steal, fold_percentage):
         }
 
 
-app = Flask('myapp', template_folder=current_folder)
-with app.app_context():
-    min_steal = 60
-    max_steal = 80
-    min_fold = 45
-    max_fold=55
+@do_profile(follow=[range_battle])
+def create_three_bet_strategy_dataviz():
+    app = Flask('myapp', template_folder=current_folder)
+    with app.app_context():
+        min_steal = 30
+        max_steal = 70
+        min_fold = 25
+        max_fold = 45
 
-    html_page = render_template(
-        'three_bet_strategy.html',
-        data_points=[
-            hand_as_json
-            for steal in range(min_steal, max_steal+1, 1)
-            for fold_percentage in range(min_fold, max_fold, 1)
-            for hand_as_json in generate_hand_properties(steal=steal, fold_percentage=fold_percentage)
-        ],
-        min_steal=min_steal,
-        max_steal=max_steal,
-        min_fold=min_fold,
-        max_fold=max_fold
-    )
+        html_page = render_template(
+            'three_bet_strategy.html',
+            data_points=[
+                hand_as_json
+                for steal in range(min_steal, max_steal + 1, 1)
+                for fold_percentage in range(min_fold, max_fold, 1)
+                for hand_as_json in generate_hand_properties(steal=steal, fold_percentage=fold_percentage)
+            ],
+            min_steal=min_steal,
+            max_steal=max_steal,
+            min_fold=min_fold,
+            max_fold=max_fold
+        )
 
-    with open(current_folder + '/generated_three_bet_strategy.html', 'w') as fp:
-        fp.write(html_page)
+        with open(current_folder + '/generated_three_bet_strategy.html', 'w') as fp:
+            fp.write(html_page)
+
+create_three_bet_strategy_dataviz()

543b9a678de457a994eab02daddcb932a967e96a
Rodolphe <rodolphe@lalilo.com>
2018-10-26 21:59:52 +0200

    steal and three bet sizes

--- three-bet-EV-versus-call-ev/app.py
+++ three-bet-EV-versus-call-ev/app.py
@@ -13,11 +13,14 @@ def hello():
     steal = int(request.json['steal'])
     fold3bet = int(request.json['fold3bet'])
     fourbet = int(request.json['fourbet'])
+    oop_penalty = int(request.json['oop_penalty'])
+    steal_size = float(request.json['steal_size'])
+    threebet_size = float(request.json['threebet_size'])
     response = jsonify([
-        equities_of('T9o', steal, fold3bet, fourbet),
-        equities_of('JTo', steal, fold3bet, fourbet),
-        equities_of('98o', steal, fold3bet, fourbet),
-        equities_of('87o', steal, fold3bet, fourbet),
+        equities_of('T9o', steal, fold3bet, fourbet, oop_penalty, steal_size, threebet_size),
+        equities_of('JTo', steal, fold3bet, fourbet, oop_penalty, steal_size, threebet_size),
+        equities_of('98o', steal, fold3bet, fourbet, oop_penalty, steal_size, threebet_size),
+        equities_of('87o', steal, fold3bet, fourbet, oop_penalty, steal_size, threebet_size),
 
     ])
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -25,17 +28,20 @@ def hello():
     return response
 
 
-def equities_of(hand_name, steal, fold3bet, fourbet):
+def equities_of(hand_name, steal, fold3bet, fourbet, oop_penalty, steal_size, threebet_size):
     fold_equity = -1
     hand = Hand(name=hand_name,
                 combos=combos_of[hand_name[2:]])
     return {
-        "x": call_equity(hand, steal) - fold_equity,
+        "x": call_equity(hand, steal, oop_penalty, steal_size) - fold_equity,
         "y": three_bet_equity(
             hand=hand,
             steal=steal,
             fold_percentage=fold3bet,
-            fourbet=fourbet
+            fourbet=fourbet,
+            oop_penalty=oop_penalty,
+            steal_size=steal_size,
+            threebet_size=threebet_size
         ) - fold_equity,
         "hand": hand_name}
 
